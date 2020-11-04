8499b63504d687f188c47b9bebd08521834e73cb
Mimo <gunjan.mtbpaul@gmail.com>
2020-11-04 09:41:02 +0530

    new

--- Hand Detection/app.js
+++ Hand Detection/app.js
@@ -1,7 +1,15 @@
+const modelParams = {
+  flipHorizontal: true, // flip e.g for video
+  imageScaleFactor: 0.7, // reduce input image size for gains in speed.
+  maxNumBoxes: 20, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.79, // confidence threshold for predictions.
+};
+
 navigator.getUserMedia = navigator.getUserMedia;
 
 // select everything in my html
-const video = document.querySelector("#ideo");
+const video = document.querySelector("#video");
 const audio = document.querySelector("#audio");
 const canvas = document.querySelector("#canvas");
 const context = canvas.getContext("2d");
@@ -13,13 +21,26 @@ handTrack.startVideo(video).then((status) => {
       { video: {} },
       (stream) => {
         video.srcObject = stream;
+        //setInterval(runDetection, 1000);
+        runDetection();
       },
       (error) => console.log(error)
     );
   }
 });
 
+function runDetection() {
+  model.detect(video).then((prediction) => {
+    console.log(prediction);
+    // model.renderPredictions(prediction, canvas, context, video);
+    if (prediction.length > 0) {
+      audio.play();
+    }
+    requestAnimationFrame(runDetection);
+  });
+}
+
 // Load the model.
-handTrack.load().then((lmodel) => {
+handTrack.load(modelParams).then((lmodel) => {
   model = lmodel;
 });
